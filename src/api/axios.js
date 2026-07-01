b1bd6484cf79dec6be6bf424a1d4c822eebd6255
Hostine12 <hostinefdg@gmail.com>
2026-07-01 17:46:04 +0100

    Utilisation de VITE_API_URL pour baseURL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,28 +1,20 @@
 // src/api/axios.js
 import axios from 'axios';
 
-// 1. On crée d'abord l'instance proprement
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api', 
+  baseURL: import.meta.env.VITE_API_URL, // ✅ utilise la variable d'environnement
   headers: {
     'Content-Type': 'application/json',
-    'Accept': 'application/json',
+    Accept: 'application/json',
   },
-  withCredentials: false
 });
 
-// 2. On configure l'intercepteur après la création
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
   return config;
-}, (error) => {
-  return Promise.reject(error);
-});
-
-// 3. On définit et exporte les variables globales à la toute fin
-export const API_BASE_URL = 'http://127.0.0.1:8000/api';
+}, (error) => Promise.reject(error));
 
 export default api;
